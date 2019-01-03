e8cfa490373b91663be56dcc2835025c97d8c454
chjjh0 <chjjh0@gmail.com>
2019-01-03 15:06:45 +0900

    0103

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+// .env 활성화
+// moongoose로 DB 연결
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const {
+    PORT: port = 4000,  // 값이 존재하지 않는다면 4000을 기본 값으로 사용
+    MONGO_URI: mongoURI
+} = process.env;
+
+// Node의 Promise를 사용하도록 설정
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURI).then(() => {
+    console.log('connected to mongodb');
+}).catch((e) => {
+    console.error(e);
+});
+
 // import middleware
 const Koa = require('koa');
 const Router = require ('koa-router');
@@ -20,6 +38,6 @@ app.use(bodyParser());
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-    console.log('listening to port 4000');
+app.listen(port, () => {
+    console.log('listening to port', port);
 });
