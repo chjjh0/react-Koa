9185c0efd5938e844968f9866f975a627fe75bb3
chjjh0 <chjjh0@gmail.com>
2019-01-03 20:15:01 +0900

    0103

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,7 +1,6 @@
 const { ObjectId } = require('mongoose').Types;
 const Post = require('models/post');
-
-
+const Joi = require('joi');
 
 // objectId 검증
 exports.checkObjectId = (ctx, next) => {
@@ -11,7 +10,6 @@ exports.checkObjectId = (ctx, next) => {
     // ID가 자릿수가 맞지 않을 때 발생
     // 자릿수는 맞는데 없는 ID면 bad request 발생
     if(!ObjectId.isValid(id)) {
-        console.log('fail');
         ctx.status = 400;
         return null;
     }
@@ -24,6 +22,22 @@ exports.checkObjectId = (ctx, next) => {
 // POST /api/postMessage
 // { title, body, tags }
 exports.write = async (ctx) => {
+    // 객체가 빈 값인지 검증
+    const schema = Joi.object().keys({
+        // required()가 붙으면 필수항목이라는 의미
+        title: Joi.string().required(),
+        body: Joi.string().required(),
+        tags: Joi.array().items(Joi.string()).required() // 문자열 배열
+    });
+    // parameter convention (검증할 객체, schema)
+    const result = Joi.validate(ctx.request.body, schema);
+
+    // error 응답
+    if(result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
     const { title, body, tags } = ctx.request.body;
 
     // Create New Post Instance
@@ -31,6 +45,8 @@ exports.write = async (ctx) => {
     const post = new Post({
         title, body, tags
     });
+
+    
     
     try {
         // DB에 등록/저장
@@ -46,10 +62,39 @@ exports.write = async (ctx) => {
 
 // GET /api/posts
 exports.list = async (ctx) => {
+    // default page 수는 1
+    // query는 문자열이므로 숫자로 형변환
+    const page = parseInt(ctx.query.page || 1, 10);
+
+    // 잘못된 페이지가 주어지면 error
+    if(page<1) {
+        ctx.status = 400;
+        return;
+    }
+
     try {
         // find() 함수 후에 exec()를 붙여줘야 서버에 쿼리를 요청
-        const posts = await Post.find().exec();
-        ctx.body = posts;
+        // sort() _id를 기준으로 -1: 내림차순 1: 오름차순
+        // limit() 보이는 개수 제한 10개씩 보이기
+        // skip() 
+        //   1페이지면 1-1 = 0 * 10 = 0이므로 0개 뛰어 넘어 limit 함수에서 지정한 10개만큼 표시
+        //   2페이지면 2-1 = 1 * 10 = 10이므로 10개 뛰어 넘어 10개만큼 표시하기에, 20번째부터 10개 표시 됨
+        // lean() 반환형을 JSON형태로 반환
+        const posts = await Post.find()
+            .sort({_id:-1})
+            .limit(10)
+            .skip((page - 1) * 10)
+            .lean()
+            .exec();
+        // 마지막 페이지 알려주기
+        const postCount = await Post.count().exec();
+        const limitBodyLength = post => ({
+            ...post,
+            body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`
+        });
+        // ctx.set은 response header를 설정
+        ctx.set('Last-Page', Math.ceil(postCount /10));
+        ctx.body = posts.map(limitBodyLength);
     } catch(e) {
         ctx.throw(e, 500);
     }
